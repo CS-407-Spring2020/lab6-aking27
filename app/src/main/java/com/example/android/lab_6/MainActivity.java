d602f209c1ca29fff28f7a83d97948ca07248fc0
Andy <aking27@wisc.edu>
2020-03-05 18:26:17 -0600

    Final commit

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -19,8 +19,8 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity { //FragmentActivity   AppCompatActivity
 
-    //Somewhere in Australia
-    private final LatLng mDestinationLatLng = new LatLng(-33.8523341, 151.2106085);
+
+    private final LatLng mDestinationLatLng = new LatLng(43.075464, -89.404336);
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient; // save the instance
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
@@ -49,14 +49,17 @@ public class MainActivity extends FragmentActivity { //FragmentActivity   AppCom
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
-        //if permission grganted, display marler at current location
+        //if permission granted, display marker at current location
         else {
             mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
                 Location mLastKnownLocation = task.getResult();
+                LatLng lastKnown = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
                 if(task.isSuccessful() && mLastKnownLocation != null){
                     mMap.addPolyline(new PolylineOptions().add(
                         new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()),
                                 mDestinationLatLng));
+
+                    mMap.addMarker(new MarkerOptions().position(lastKnown).title("Current Location"));
                 }
             });
         }
